906924404e8818a7c30784ad2d46c6355ab67c5f
AhmedEissaeng <130767408+AhmedEissaeng@users.noreply.github.com>
2026-05-28 20:33:10 +0300

    Update file.py

--- file.py
+++ file.py
@@ -79,7 +79,7 @@ HTML_PAGE = """
 
 @app.route('/')
 def home():
-    return render_template_string(HTML_PAGE)
+    return "<h1>Flask Docker App Working 🚀</h1>"
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.run(host='0.0.0.0', port=5001)
